Fixing consensus GP term calculation. Train API now has a simultaneous GD option

models/dcgan_consensus.py
@@ -11,8 +11,7 @@ init - stddev 0.02
 '''
 
 class DCGAN_CONS(BaseModel):
-    def __init__(self, name, training, D_lr=2e-4, G_lr=2e-4, image_shape=[64, 64, 3], z_dim=100):
-        self.beta1 = 0.5
+    def __init__(self, name, training, D_lr=1e-4, G_lr=1e-4, image_shape=[64, 64, 3], z_dim=100):
         super(DCGAN_CONS, self).__init__(name=name, training=training, D_lr=D_lr, G_lr=G_lr,
             image_shape=image_shape, z_dim=z_dim)
 
@@ -37,28 +36,26 @@ class DCGAN_CONS(BaseModel):
             # Consensus Optimization Regularization term -- Calculated for per-batch loss
             gamma = 10
 
-            D_grad = tf.gradients(ys = D_loss, xs = D_vars)[0] # gradient of D_loss
-            D_grad_norm = tf.norm(slim.flatten(D_grad), axis=None)  # l2 norm
-            D_norm_square = tf.square(D_grad_norm)
-            G_grad = tf.gradients(G_loss, G_vars)[0] # gradient of D_loss
-            G_grad_norm = tf.norm(slim.flatten(G_grad),axis=None)
-            G_norm_square = tf.square(G_grad_norm)
+            # D_loss/G_loss is already the average loss over the batch.
+            D_grad = tf.gradients(ys = D_loss, xs = D_vars) # gradient of D_loss
+            G_grad = tf.gradients(ys = G_loss,xs = G_vars) # gradient of G_loss
+            D_grad_sq = sum([tf.reduce_sum(tf.square(g)) for g in D_grad])
+            G_grad_sq = sum([tf.reduce_sum(tf.square(g)) for g in G_grad])
+            L_reg = D_grad_sq + G_grad_sq
 
-            L_reg = D_norm_square + G_norm_square
+
             D_loss += gamma*L_reg
             G_loss += gamma*L_reg
 
 
-            # Simulataeneous GD
             D_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=self.name+'/D/')
             G_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope=self.name+'/G/')
 
             with tf.control_dependencies(D_update_ops):
-                D_train_op = tf.train.AdamOptimizer(learning_rate=self.D_lr, beta1=self.beta1).\
+                D_train_op = tf.train.RMSPropOptimizer(learning_rate=self.D_lr).\
                     minimize(D_loss, var_list=D_vars)
             with tf.control_dependencies(G_update_ops):
-                # learning rate 2e-4/1e-3
-                G_train_op = tf.train.AdamOptimizer(learning_rate=self.G_lr, beta1=self.beta1).\
+                G_train_op = tf.train.RMSPropOptimizer(learning_rate=self.G_lr).\
                     minimize(G_loss, var_list=G_vars, global_step=global_step)
 
             # summaries

train.py
@@ -22,7 +22,7 @@ def build_parser():
     parser.add_argument('--ckpt_step', default=5000, help='# of steps for saving checkpoint (default: 5000)', type=int)
     parser.add_argument('--renew', action='store_true', help='train model from scratch - \
         clean saved checkpoints and summaries', default=False)
-
+    parser.add_argument('--simultaneous', action='store_true', help='Choose between alternate GD and simultaeneous GD', default=False)
     return parser
 
 
@@ -36,7 +36,7 @@ def sample_z(shape):
     return np.random.normal(size=shape)
 
 
-def train(model, dataset,input_op, num_epochs, batch_size, n_examples, ckpt_step, renew=False):
+def train(model, dataset,input_op, num_epochs, batch_size, n_examples, ckpt_step, renew=False,simultaneous = False):
     # n_examples = 202599 # same as util.num_examples_from_tfrecords(glob.glob('./data/celebA_tfrecords/*.tfrecord'))
     # 1 epoch = 1583 steps
     print("\n# of examples: {}".format(n_examples))
@@ -120,8 +120,11 @@ def train(model, dataset,input_op, num_epochs, batch_size, n_examples, ckpt_step
                 batch_X = sess.run(input_op)
                 batch_z = sample_z([batch_size, model.z_dim])
 
-                _, summary = sess.run([model.D_train_op, summary_op], {model.X: batch_X, model.z: batch_z})
-                _, global_step = sess.run([model.G_train_op, model.global_step], {model.X: batch_X, model.z: batch_z})
+                if simultaneous is False:
+                    _, summary = sess.run([model.D_train_op, summary_op], {model.X: batch_X, model.z: batch_z})
+                    _, global_step = sess.run([model.G_train_op, model.global_step], {model.X: batch_X, model.z: batch_z})
+                else:
+                    _,_,summary,global_step = sess.run([model.D_train_op,model.G_train_op,summary_op,model.global_step], {model.X: batch_X, model.z: batch_z})
 
                 summary_writer.add_summary(summary, global_step=global_step)
 
@@ -173,4 +176,4 @@ if __name__ == "__main__":
     resized_image_shape = [64,64,3]
     model = config.get_model(FLAGS.model, FLAGS.name, training=True,image_shape=resized_image_shape)
     train(model=model, dataset=FLAGS.dataset,input_op=X, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size,
-        n_examples=n_examples, ckpt_step=FLAGS.ckpt_step, renew=FLAGS.renew)
+        n_examples=n_examples, ckpt_step=FLAGS.ckpt_step, renew=FLAGS.renew,simultaneous = FLAGS.simultaneous)